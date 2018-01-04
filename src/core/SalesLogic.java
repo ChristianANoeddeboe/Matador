823c8639fe1fbb4bdfbb9336ce6d56f0b6034cc2
SimonFritzH <32448409+SimonFritzH@users.noreply.github.com>
2018-01-04 13:59:55 +0100

    SalesLogic changes

--- src/core/SalesLogic.java
+++ src/core/SalesLogic.java
@@ -1,5 +1,21 @@
 package core;
 
 public class SalesLogic {
-
+	
+	public static String sellHouse(int id, Player currentPlayer) {
+		Entities entities = Entities.getInstance();
+		Normal[] fields = (Normal[]) entities.getFieldArr();
+		
+		currentPlayer.getAccount().deposit(fields[id].getHousePrice());
+		return "SoldHouse, "+ fields[id].getHousePrice();
+	}
+	
+	public static String pawnProperty(int id, Player currentPlayer) {
+		Entities entities = Entities.getInstance();
+		Property[] fields = (Property[]) entities.getFieldArr();
+		
+		fields[id].setIsPawned(true);
+		currentPlayer.getAccount().deposit(fields[id].getPawnValue());
+		return "Pawned, " + fields[id].getPawnValue();
+	}
 }
