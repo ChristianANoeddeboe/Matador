ed9c823645764b789ef2586c16ad50a53aa692ae
SimonFritzH <32448409+SimonFritzH@users.noreply.github.com>
2018-01-04 12:42:14 +0100

    BuyLogic updates

--- src/core/BuyLogic.java
+++ src/core/BuyLogic.java
@@ -9,17 +9,24 @@ public class BuyLogic {
 	}
 	
 	public static String houseBuyLogic(int id, Player currentPlayer) {
-		if(currentPlayer.getAccount().canAfford(FieldArr[id].getBuildPrice)) {
+		// Player can buy new house and there is no more than 4 buildings on the field
+		if(currentPlayer.getAccount().canAfford(FieldArr[id].getBuildPrice && FieldArr[id].getHouseNumber < 5)) {
 			currentPlayer.getAccount().getBalance().withdraw(FieldArr[id].getBuildPrice);
 			FieldArr[id].houseNumber++;
 			FieldArr[id].setCurrentPrice(calcHousePrice(id, FieldArr[id].houseNumber));
 			return "HouseBought";
 		}
+		// There is already a hotel (5 houses) on the field
+		else if(FieldArr[id].getHouseNumber == 5) {
+			return "TooManyHouses";
+		}
+		// The Player cannot afford another house
 		else {
 			return "CannotAfford, "+FieldArr[id].getBuildPrice();
 		}
 	}
 	
+	// Calculates the new price of rent when a new house has been build
 	private static int calcHousePrice(int id, int houses) {
 		switch (houses) {
 		case 1:
