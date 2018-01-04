2ca8b3ea5e517dbf465b05c42595636c636abadf
NK <NK@NK-XPS.eduroam.wireless.dtu.dk>
2018-01-04 10:45:36 +0100

    changed normal field

--- src/core/Normal.java
+++ src/core/Normal.java
@@ -1,10 +1,15 @@
 package core;
 
 public class Normal extends Property {
-	protected int housePrice;
-	protected int hotelPrice;
-	protected String colour = "";
-	public Normal(int id, String name, String type, Player owner, int baseValue, int pawnValue, int houseprice, int hotelprice, String colour) {
+	private int house1Price;
+	private int house2Price;
+	private int house3Price;
+	private int house4Price;
+	private int BuildPrice;
+	private int hotelPrice;
+	private String colour = "";
+	
+	public Normal(int id, String name, String type, Player owner, int baseValue, int pawnValue, int house1Price, int house2Price, int house3Price, int house4Price, int buildPrice, int hotelprice, String colour) {
 		super(id, name, type, owner, baseValue, pawnValue);
 		this.id = id;
 		this.name = name;
@@ -14,15 +19,69 @@ public class Normal extends Property {
 		this.currentValue = baseValue;
 		this.pawnValue = pawnValue;
 		this.isPawned = false;
-		this.housePrice = houseprice;
+		this.house1Price = house1Price;
+		this.house2Price = house2Price;
+		this.house3Price = house3Price;
+		this.house4Price = house4Price;
 		this.hotelPrice = hotelprice;
+		this.BuildPrice = buildPrice;
 		this.colour = colour;
 		// TODO Auto-generated constructor stub
 	}
 	
 
-	private int getHousePrice() { return housePrice; }
-	private void setHousePrice(int value) { housePrice = value; }
+	public int getHouse1Price() {
+		return house1Price;
+	}
+
+
+	public void setHouse1Price(int house1Price) {
+		this.house1Price = house1Price;
+	}
+
+
+	public int getHouse2Price() {
+		return house2Price;
+	}
+
+
+	public void setHouse2Price(int house2Price) {
+		this.house2Price = house2Price;
+	}
+
+
+	public int getHouse3Price() {
+		return house3Price;
+	}
+
+
+	public void setHouse3Price(int house3Price) {
+		this.house3Price = house3Price;
+	}
+
+
+	public int getHouse4Price() {
+		return house4Price;
+	}
+
+
+	public void setHouse4Price(int house4Price) {
+		this.house4Price = house4Price;
+	}
+
+
+	public int getBuildPrice() {
+		return BuildPrice;
+	}
+
+
+	public void setBuildPrice(int buildPrice) {
+		BuildPrice = buildPrice;
+	}
+
+
+	private int getHousePrice() { return house1Price; }
+	private void setHousePrice(int value) { house1Price = value; }
 	private int getHotelPrice() { return hotelPrice; }
 	private void setHotelPrice(int value) { hotelPrice = value; }
 	private String getColour() { return colour; }
