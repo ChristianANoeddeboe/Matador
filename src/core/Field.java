9f2b7e02b6408dac823d935b4eb4e4cfa0abca30
Angercraft <angercraft@gmail.com>
2018-01-04 11:29:36 +0100

    Changed visibility

--- src/core/Field.java
+++ src/core/Field.java
@@ -2,13 +2,24 @@ package core;
 
 public abstract class Field {
 
-	protected String name = "";
-	protected int id = 0;
-	protected String type = "";
-	public String getName() { return name; }
-	public void setName(String n) { name = n; }
-	public int getId() { return id; }
-	public void setId(int i) { id = i; }
+	private String name;
+	private int id;
+	private String type;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String n) {
+		name = n;
+	}
+
+	public int getId() {
+		return id;
+	}
+	public void setId(int i) {
+		id = i;
+	}
 	
 	public Field(int id, String name, String type) {
 		this.id = id;
