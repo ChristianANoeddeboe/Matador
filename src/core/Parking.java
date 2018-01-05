ef5f7e1ba99f2138532d25480ed7118f4a3c8ca6
Angercraft <Angercraft@gmail.com>
2018-01-05 11:30:03 +0100

    Added a description attribute.

--- src/core/Parking.java
+++ src/core/Parking.java
@@ -2,8 +2,8 @@ package core;
 
 public class Parking extends Field {
 
-	public Parking(int id, String name) {
-		super(id, name);
+	Parking(int id, String name, String description) {
+		super(id, name, description);
 		// TODO Auto-generated constructor stub
 	}
 
