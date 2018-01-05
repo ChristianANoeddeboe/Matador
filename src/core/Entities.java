846eeb90f5062fd2e9a4f84c268d21d6efeb3c47
Mathias Thejsen <thejsen@live.dk>
2018-01-05 15:56:04 +0100

    Fixed entities error

--- src/core/Entities.java
+++ src/core/Entities.java
@@ -14,42 +14,42 @@ public class Entities {
 	private Dice diceArr[];
 	private int chanceCardArr[]; 
 	private Player playerArr[];
-	
+
 	private Entities() {
 		//load config and translation
 		config = new PropertiesIO("config.properties");
 		// translate = new PropertiesIO("translate.properties");
-		
+
 		//initialize fields, dies and chanceCards
 		initFields();
 		initDies(2);
 		initChanceCards(32);
-		
+
 		/* 
 		 * PlayerArr will only be initialized with a call to the public function: initPlayers 
 		 */
 	}
-	
-    private void initChanceCards(int amountOfChanceCards) {
-    	chanceCardArr = new int[amountOfChanceCards];
+
+	private void initChanceCards(int amountOfChanceCards) {
+		chanceCardArr = new int[amountOfChanceCards];
 		for (int i = 0 ; i < chanceCardArr.length ; i++)
-	    {
+		{
 			int rndNumber;
 			boolean newCard;
-			
+
 			do {
 				newCard = true;
 				rndNumber = ThreadLocalRandom.current().nextInt(0, chanceCardArr.length);
-				
+
 				for (int j = 0 ; j < i ; j++)
 					if(chanceCardArr[j] == rndNumber) {
 						newCard = false;
 						break;
 					}
 			} while (!newCard);
-			
+
 			chanceCardArr[i] = rndNumber;
-	    }
+		}
 	}
 
 	private void initDies(int amountOfDies) {
@@ -59,12 +59,12 @@ public class Entities {
 	}
 
 	public static Entities getInstance()
-    {
-        if (entities == null)
-            entities = new Entities();
- 
-        return entities;
-    }
+	{
+		if (entities == null)
+			entities = new Entities();
+
+		return entities;
+	}
 
 	private void initFields() {
 		fieldArr = new Field[40];
@@ -74,31 +74,31 @@ public class Entities {
 			String[] descriptionSplit;
 
 			// START FIELD
-			if(i == 0)
+			if(i == 0) {
 				description = config.getTranslation("field"+(i+1)+"description");
 				descriptionSplit = description.split(",");
 				description = descriptionSplit[0]+config.getTranslation("field"+(i+1)+"value")+descriptionSplit[1];
 				fieldArr[i] = new Start(i,
 						config.getTranslation("field"+(i+1)),
 						description);
-			
+			}
 			// NORMAL FIELDS
 			if (i == 1 || i == 3 || i == 6 || i == 8 || i == 9 || i == 11 || i == 13 || i == 14 || i == 16 || i == 18 || i == 19 || i == 21 || i == 23 || i == 24 || i == 26 || i == 27 || i == 29 || i == 31 || i == 32 || i == 34 || i == 37 || i == 39) {
 				Color color;
 				switch(config.getTranslation("field"+(i+1)+"color")) {
-					default : color = Color.black; break;
-					case "gul" : color = Color.yellow; break;
-					case "blå" : color = Color.blue; break;
-					case "pink" : color = Color.pink; break;
-					case "brun"	: color = Color.orange; break;
-					case "hvid" : color = Color.white; break;
-					case "grøn" : color = Color.green; break;
-					case "lilla" : color = Color.magenta; break;
-					case "rød" : color = Color.red; break;
-					case "grå" : color = Color.gray; break;
-					
+				default : color = Color.black; break;
+				case "gul" : color = Color.yellow; break;
+				case "blå" : color = Color.blue; break;
+				case "pink" : color = Color.pink; break;
+				case "brun"	: color = Color.orange; break;
+				case "hvid" : color = Color.white; break;
+				case "grøn" : color = Color.green; break;
+				case "lilla" : color = Color.magenta; break;
+				case "rød" : color = Color.red; break;
+				case "grå" : color = Color.gray; break;
+
 				}
-				
+
 				int housePrices[] = { 
 						Integer.parseInt(config.getTranslation("field"+(i+1)+"value")),
 						Integer.parseInt(config.getTranslation("field"+(i+1)+"hus1")), 
@@ -107,7 +107,7 @@ public class Entities {
 						Integer.parseInt(config.getTranslation("field"+(i+1)+"hus4")), 
 						Integer.parseInt(config.getTranslation("field"+(i+1)+"hotel"))
 				};
-				
+
 				fieldArr[i] = new Normal(i,
 						config.getTranslation("field"+(i+1)), 
 						null,
@@ -118,7 +118,7 @@ public class Entities {
 						color,
 						"test");
 			}
-			
+
 			// SHIPPING FIELDS
 			if(i == 5 || i == 15 || i == 25 || i == 35) {
 				fieldArr[i] = new Shipping(i,
@@ -128,7 +128,7 @@ public class Entities {
 						Integer.parseInt(config.getTranslation("field"+(i+1)+"pant")),
 						"test");
 			}
-			
+
 			// BREWERY FIELDS
 			if(i == 12 || i == 28) {
 				fieldArr[i] = new Brewery(i,
@@ -138,14 +138,14 @@ public class Entities {
 						Integer.parseInt(config.getTranslation("field"+(i+1)+"pant")),
 						"test");
 			}
-			
+
 			// CHANCE CARD FIELDS
 			if(i == 2 || i == 7 || i == 17 || i == 22 || i == 33 || i == 36) {
 				fieldArr[i] = new Chance(i,
 						config.getTranslation("field"+(i+1)),
 						"test");
 			}
-			
+
 			//TAX FIELDS
 			if(i == 4 || i == 38) {
 				fieldArr[i] = new Tax(i,
@@ -153,25 +153,25 @@ public class Entities {
 						Integer.parseInt(config.getTranslation("field"+(i+1)+"value")),
 						"test");
 			}
-			
+
 			//PRISON FIELD
 			if(i == 10 || i == 30) {
 				fieldArr[i] = new Prison(i,
 						config.getTranslation("field"+(i+1)),
 						"test");
 			}
-			
+
 			//Parking FIELD
 			if(i == 20) {
 				fieldArr[i] = new Parking(i,
 						config.getTranslation("field"+(i+1)),
 						"test");
 			}
-			
-			
+
+
 		}
 	}
-	
+
 	public void initPlayers(int amountOfPlayers) {
 		playerArr = new Player[amountOfPlayers];
 	}
@@ -223,7 +223,7 @@ public class Entities {
 	public void setChanceCardArr(int[] chanceCardArr) {
 		this.chanceCardArr = chanceCardArr;
 	}
-	
+
 }
 
 
