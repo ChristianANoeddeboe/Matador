ce901497625d42a79d58072ef215ab8e23ae6e77
SimonFritzH <32448409+SimonFritzH@users.noreply.github.com>
2018-01-08 12:43:42 +0100

    SalesLogic updates

--- src/core/SalesLogic.java
+++ src/core/SalesLogic.java
@@ -1,4 +1,7 @@
 package core;
+
+import java.awt.Color;
+
 /**
  * 
  * @author Mathias Thejsen s175192 && Simon Hansen s175191
@@ -28,10 +31,63 @@ public class SalesLogic {
 	 * @param currentPlayer
 	 * @return
 	 */
-	protected String pawnProperty(Player currentPlayer) { 
+	protected String pawnProperty(Player currentPlayer) {
 		Property[] fields = (Property[]) entities.getFieldArr();
+		if(fields[id] instanceof Normal) {
+			Normal[] nfields = (Normal[]) entities.getFieldArr();
+			if(nfields[id].getHouseCounter() == 0 && checkHouses(currentPlayer) && nfields[id].getHouseCounter() == 0) {
+				fields[id].setIsPawned(true); // We set the pawn bool to true
+				currentPlayer.getAccount().deposit(fields[id].getPawnValue()); // Weget the pawn value and deposit it into the owners account
+				return "Pawned, " + fields[id].getPawnValue();
+			}
+			else {
+				return "RemoveHouses";
+			}
+		}
 		fields[id].setIsPawned(true); // We set the pawn bool to true
 		currentPlayer.getAccount().deposit(fields[id].getPawnValue()); // Weget the pawn value and deposit it into the owners account
 		return "Pawned, " + fields[id].getPawnValue();
+	}
+	
+	protected boolean checkHouses(Player currentPlayer) {
+		Normal[] fields = (Normal[]) entities.getFieldArr();
+		Color propertyColor = fields[id].getColour();
+		
+		switch (propertyColor.toString()) {
+		case "Color.blue":
+			Normal[] blueFields = (Normal[]) entities.getNormalBlue();
+			for(int i = 0; i < blueFields.length; i++) {
+				if(blueFields[i].getOwner() == fields[id].getOwner()) {
+					if(blueFields[i].getHouseCounter() > fields[id].getHouseCounter() + 1) {
+						return false;
+					}
+					else {
+						return true;
+					}
+				}
+				else {
+					return true;
+				}
+			}
+		case "Color.pink":
+			Normal[] pinkFields = (Normal[]) entities.getNormalBlue();
+			for(int i = 0; i < pinkFields.length; i++) {
+				if(pinkFields[i].getOwner() == fields[id].getOwner()) {
+					if(pinkFields[i].getHouseCounter() > fields[id].getHouseCounter() + 1) {
+						return false;
+					}
+					else {
+						return true;
+					}
+				}
+				else {
+					return true;
+				}
+			}
+		default:
+			break;
+		}
+		
+		return true;
 	}
 }
