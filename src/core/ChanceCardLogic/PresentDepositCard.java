2e5b0108b1ac1d6a6bd697d079d39ef7b522edf2
MagnusKochDTU <s175189@student.dtu.dk>
2018-01-09 14:46:44 +0100

    ChanceCardController is now fully implemented small changes can occur later though and changes to config is changed in connection with ChanceCardController

---
+++ src/core/ChanceCardLogic/PresentDepositCard.java
@@ -0,0 +1,10 @@
+package core.ChanceCardLogic;
+
+/**
+ * Created by magnus
+ */
+public class PresentDepositCard extends ChanceCard {
+    public PresentDepositCard(int id, String description) {
+        super(id, description);
+    }
+}
