2a6cc3f7120b9a4cf287369fd59bb0dea66cd395
SimonFritzH <32448409+SimonFritzH@users.noreply.github.com>
2018-01-11 09:51:47 +0100

    GUI message updates

--- src/core/StreetController.java
+++ src/core/StreetController.java
@@ -56,7 +56,7 @@ public class StreetController {
 				
 				// If field is owned by someone else, we check if they can afford landing there
 				if(currentPlayer.getAccount().canAfford(street.getRentValue())) { 
-					
+					guiController.writeMessage("You landed on.."+street.getName() + "..'s field and have to pay.."+street.getRentValue()+" to "+street.getOwner().getName());
 					// Withdraw rentValue from the player
 					currentPlayer.getAccount().withdraw(street.getRentValue());
 					
@@ -65,7 +65,7 @@ public class StreetController {
 					
 					// Sends updates to GUIController
 					guiController.updatePlayerBalance(street.getOwner().getGuiId(), street.getOwner().getAccount().getBalance());
-					guiController.writeMessage("You landed on.."+street.getOwner().getName() + "..'s field and had to pay.."+street.getRentValue());
+
 					
 				}
 				else { // Can't afford
