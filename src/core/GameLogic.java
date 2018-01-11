4a9b35800d36133ce57895168040b8219f3b235c
Mathias Thejsen <thejsen@live.dk>
2018-01-11 11:02:02 +0100

    updated for ismoved

--- src/core/GameLogic.java
+++ src/core/GameLogic.java
@@ -85,6 +85,11 @@ public class GameLogic {
 					checkIfExtraRound(currentPlayer);
 					
 					findLogic(currentPlayer, diceCup);
+					
+					if(currentPlayer.isMoved()) {
+						findLogic(currentPlayer, diceCup);
+						currentPlayer.setMoved(true);
+					}
 
 					return false;
 				}
