223844c02419f1c5bca771789270d0914e40a873
Angercraft <angercraft@gmail.com>
2018-01-11 12:03:36 +0100

    Fixed player not being released when using prison card.

--- src/core/PropertiesIO.java
+++ src/core/PropertiesIO.java
@@ -9,14 +9,18 @@ import java.util.Properties;
  * Translator is being handled using a properties file.
  */
 public class PropertiesIO {
-    private Properties translations = new Properties();
+
+    private static Properties translations = new Properties();
+    private static Properties config = new Properties();
+
     /**
      * Translator constructor
      * @throws IOException may throw exception if file is not found
      */
-    public PropertiesIO (String name) {
+    private PropertiesIO () {
         try {
-            translations.load(ClassLoader.getSystemResourceAsStream(name));
+            translations.load(ClassLoader.getSystemResourceAsStream("translations.properties"));
+            config.load(ClassLoader.getSystemResourceAsStream("config.properties"));
         } catch (FileNotFoundException e) {
             System.out.println("File not found: "+e);
         } catch (IOException e) {
@@ -28,8 +32,12 @@ public class PropertiesIO {
      * @param toTranslate the key to look for
      * @return The attached string to the key
      */
-    public String getTranslation (String toTranslate) {
+    public static String getTranslation (String toTranslate) {
         return translations.getProperty(toTranslate);
+    }
+
+    public static String getConfig (String toTranslate) {
+        return config.getProperty(toTranslate);
     }
 
 }
