f75cfb79ff55dcf09e820582ed183ec0b187b582
Angercraft <angercraft@gmail.com>
2018-01-11 12:23:54 +0100

    Cleaned

--- src/core/Prison.java
+++ src/core/Prison.java
@@ -4,11 +4,10 @@ package core;
  * @author Mathias Thejsen s175192 && Simon Hansen s175191
  *
  */
-public class Prison extends SpecialProperty {
+class Prison extends SpecialProperty {
 
-	public Prison(int id, String name, String description) {
+	Prison(int id, String name, String description) {
 		super(id, name, description);
-		// TODO Auto-generated constructor stub
 	}
 
 }
