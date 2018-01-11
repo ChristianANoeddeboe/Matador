08aa79f923cc4859c4cc8666c0e4be774ca9ffdf
SimonFritzH <32448409+SimonFritzH@users.noreply.github.com>
2018-01-11 09:51:47 +0100

    GUI message updates

--- src/core/ShippingController.java
+++ src/core/ShippingController.java
@@ -64,7 +64,7 @@ public class ShippingController {
 				
 				// Check if the player can afford the rent
 				if(currentPlayer.getAccount().canAfford(rentPrice)) {
-					
+					guiController.writeMessage("You landed on.."+shipping.getName() + "..'s field and have to pay.."+shipping.getRentValue()+" to "+shipping.getOwner().getName());
 					// Withdraw rentPrice from player
 					currentPlayer.getAccount().withdraw(rentPrice);
 					
@@ -73,7 +73,6 @@ public class ShippingController {
 					
 					// Send updates to GUIController
 					guiController.updatePlayerBalance(shipping.getOwner().getGuiId(), shipping.getOwner().getAccount().getBalance());
-					guiController.writeMessage("You landed on.."+shipping.getOwner().getName() + "..'s field and had to pay.."+shipping.getRentValue()); 
 					
 				}
 				else { // We can't afford landing here
