5d8f94b81db153c748253f671b4af23291f486c9
Mathias Thejsen <thejsen@live.dk>
2018-01-11 14:27:17 +0100

    Translations

--- src/core/SpecialProperty.java
+++ src/core/SpecialProperty.java
@@ -1,5 +1,9 @@
 package core;
-
+/**
+ * 
+ * @author Mathias s175192 og Simon
+ *
+ */
 public class SpecialProperty extends Field {
 	public SpecialProperty(int id, String name, String description) {
 		super(id, name, description);
