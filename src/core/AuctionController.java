a751245535762574396c0d09fd2d61d7aeeae746
MagnusKochDTU <s175189@student.dtu.dk>
2018-01-12 14:39:09 +0100

    Comments added on auctioncontroller

--- src/core/AuctionController.java
+++ src/core/AuctionController.java
@@ -1,7 +1,7 @@
 package core;
 
 /**
- * Created by magnus
+ * @author Magnus Stjernborg Koch - s175189
  *
  */
 public class AuctionController {
@@ -15,13 +15,22 @@ public class AuctionController {
         whoHasTheHighestBid = null;
     }
 
+    /**
+     * This function is called when a new auction needs to start. Its public so other controllers can call the function
+     */
     public void startAuction (Player playerNotIncluded, Field field, Player[] players) {
+        //auction is now started
         auctionStatus = true;
+        //amount of players in auction
         int playersInAuction = 0;
+        //The property on auction
         Property propertyOnAuction = (Property) field;
+        //initialising an array for all bidders in the current auction
         bidders = new Player[players.length];
+        //initialising the highest bid, so the default highest bid depends on the property price
         highestBid = propertyOnAuction.getBuyValue()-1;
 
+        //Loop is going through players and checking who can bet in this auction
         for (int i = 0; i < players.length; i++) {
             if (!(players[i] == playerNotIncluded))
                 if (players[i].getAccount().canAfford(highestBid)) {
@@ -30,19 +39,27 @@ public class AuctionController {
                 }
         }
 
+        //The auctions starts only if there are at least one that can bet
         if (!(playersInAuction == 0)) {
             guiController.writeMessage("Auction on card: " + field.getName());
 
+            //Runs auction until the auctionstatus is changed
             while (auctionStatus) {
                 boolean noMoreRounds = true;
+                //Goes through all bidders
                 for (Player bidder : bidders) {
                     if (!(bidder == null)) {
+                        //if player has the highest bet
                         if (!(bidder == whoHasTheHighestBid)) {
+                            //checks if bidder can afford highest bet
                             if (bidder.getAccount().canAfford(highestBid + 1)) {
                                 switch (guiController.requestPlayerChoiceButtons("Vil spilleren: " + bidder.getName() + " byde?", PropertiesIO.getTranslation("yesbutton"), PropertiesIO.getTranslation("nobutton"))) {
                                     case "Ja":
+                                        //The amount the player bets
                                         int bid = guiController.requestIntegerInput(bidder.getName() + "'s turn, Top bid: " + highestBid + ", your bid: ",0,200000);
+                                        //can player afford the bet
                                         if (bidder.getAccount().canAfford(bid)) {
+                                            //is bet higher then highest bet
                                             if (bid > highestBid) {
                                                 highestBid = bid;
                                                 whoHasTheHighestBid = bidder;
@@ -72,6 +89,7 @@ public class AuctionController {
                     auctionStatus = false;
             }
 
+            //Checks if there is a winner
             if (!(whoHasTheHighestBid == null)) {
                 wonAuction(propertyOnAuction);
                 guiController.writeMessage(whoHasTheHighestBid.getName() + " had the highest bid on " + propertyOnAuction.getName());
@@ -80,8 +98,13 @@ public class AuctionController {
         }
     }
 
+    /**
+     * When someone has won the auction house the player will be set has owner of the property
+     */
     private void wonAuction(Property propertyOnAuction) {
+        //Takes amount which the player bed from his account
         whoHasTheHighestBid.getAccount().withdraw(highestBid);
+        //Set him as the new owner of the property
         propertyOnAuction.setOwner(whoHasTheHighestBid);
         guiController.updatePlayerBalance(whoHasTheHighestBid.getGuiId(),whoHasTheHighestBid.getAccount().getBalance());
         guiController.setOwner(whoHasTheHighestBid.getGuiId(),propertyOnAuction.getId());
